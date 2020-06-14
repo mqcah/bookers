78d1730df78561a1fe435f5fa6c13394368e8909
mqcah <mqcha94@outlook.jp>
2020-06-14 16:27:02 +0900

    [finish]bookers]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,22 @@
 class BooksController < ApplicationController
   def index
-    
+    @books = Book.all
+  	@book = Book.new
   end
 
   def show
   	@book = Book.find(params[:id])
   end
 
-  def new
-    @books = Book.all
-  	@book = Book.new
-  end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-  	
+  	   @book = Book.new(book_params)
+    if @book.save
+       redirect_to book_path(@book.id)
+    else
+       @books = Book.all
+       render "index"
+    end
   end
 
   def edit
@@ -25,14 +25,18 @@ class BooksController < ApplicationController
 
   def update
   	book = Book.find(params[:id])
-  	book.update(book_params)
+  if	book.update(book_params)
   	redirect_to book_path(book.id)
+  else
+      @book = book
+      render "edit"
+    end
   end
 
 def destroy
- 	@book = find_book_by_id
- 	@book.destroy
- 	redirect_to books_path
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
  end
 
   private
